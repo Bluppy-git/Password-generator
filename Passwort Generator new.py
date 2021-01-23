2b0761b8e76a3a3f5f6e0c5d92bdacdac8be00f7
Bluppy <69403210+Bluppy-git@users.noreply.github.com>
2021-01-23 07:23:43 +0100

    Update Passwort Generator new.py

--- Passwort Generator new.py
+++ Passwort Generator new.py
@@ -1,7 +1,7 @@
 import secrets
 import string
 
-x = 8 #the x is the number of letters your password will have
-
+x = int(input("Please enter the password length: ")) #length of the password
 chars = string.digits + string.ascii_letters + string.punctuation
+
 print("".join(secrets.choice(chars) for _ in range(x)))
