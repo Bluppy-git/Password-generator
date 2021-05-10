367d33c012845a25aa91801e09b45774ebd0bd0a
Bluppy <69403210+Bluppy-git@users.noreply.github.com>
2021-05-10 20:12:21 +0200

    Delete Passwort Generator new.py

--- Passwort Generator new.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import string
-
-x = int(input("Please enter the password length: ")) #length of the password
-chars = string.digits + string.ascii_letters + string.punctuation
-
-print("".join(secrets.choice(chars) for _ in range(x)))
